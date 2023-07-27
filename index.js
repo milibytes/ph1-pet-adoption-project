a0e5fc48e4473f3e71ca88def039b2adac21df65
Bret Barden <bret.barden@gmail.com>
2023-07-27 17:59:02 -0400

    Bret 0727 6:00pm

--- index.js
+++ index.js
@@ -15,8 +15,12 @@
 const petNav = document.querySelector("#pet-nav");
 const searchForm = document.querySelector("#search-form");
 const searchInput = document.querySelector("#search-query");
+const lickButton = document.querySelector("#lick-button");
 let currentPet;
 let pets;
+
+
+
 
 // Write a function to populate pet details when filling into containers and when highlighting 
 // a particular pet on the right side display panel
@@ -27,22 +31,22 @@ function petDetails(pet) {
     currentPet = pet;
     // Set the detail photo for when someone clicks on the pet to display details
     const pngImage = document.querySelector("#image-png");
-    pngImage.src = pet.image;
+    pngImage.src = pet.pngImage; //Fixed this as was not pointing to png
     pngImage.alt = pet.name;
     const name = document.querySelector(".name");
-    name.textContent = "Name: " + pet.name;
+    name.textContent = pet.name;
     const age = document.querySelector(".age");
-    age.textContent = "Age: " + pet.age;
+    age.textContent = "AGE: " + pet.age;
     const birthday = document.querySelector(".birthday");
-    birthday.textContent = "Birthday: " + pet.birthday;
+    birthday.textContent = "BIRTHDAY: " + pet.birthday;
     const breed = document.querySelector(".breed");
-    breed.textContent = "Breed: " + pet.breed;
+    breed.textContent = "BREED: " + pet.breed;
     const tricks = document.querySelector(".tricks");
-    tricks.textContent = "Tricks: " + pet.tricks;
+    tricks.textContent = "TRICKS: " + pet.tricks;
     const species = document.querySelector(".species");
-    species.textContent = "Species: " + pet.species;
+    species.textContent = "SPECIES: " + pet.species;
     const sex = document.querySelector(".sex");
-    sex.textContent = "Sex: " + pet.sex;
+    sex.textContent = "SEX: " + pet.sex;
     // const licked = document.querySelector("#licked")
     // licked.textContent = pet.licked ? "Licked 👅 ! " : "Lick?"
     // const licks = document.querySelector("#licks")
@@ -51,6 +55,7 @@ function petDetails(pet) {
 }
 
 // Fetch request to populate the petNav from the JSON file
+function renderPets () {
 fetch("http://localhost:3000/pets")
     .then((response) => response.json())
     .then((fetchedPets) => {
@@ -89,6 +94,7 @@ fetch("http://localhost:3000/pets")
     // Call the petDetails function to display details of the array object for the random initial pet
     petDetails(initialPet)
 })
+}
 
 
 
@@ -138,212 +144,48 @@ fetch("http://localhost:3000/pets")
             }
         });
 
+//Render the pets on load
+renderPets ()
 
-//When user clicks on the Lick button, up the number of licks and do a PATCH to the database
-//to update the number of Licks
-const lickButton = document.querySelector("#lick-button");
-lickButton.addEventListener("click", () => {
-    currentPet.licks++;
-    currentPet.licked = !currentPet.licked;
-    fetch(`http://localhost:3000/${currentPet}`, {
-        method: "PATCH",
-        headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json",
-        },
-        body: JSON.stringify({
-            licks: currentPet.licks,
-            licked: currentPet.licked,
-        }),
-    })
-        .then((response) => response.json())
-        .then((patchedPet) => (licks.textContent = `${patchedPet.licks} Licks`));
+ 
+
+// Hide the adoption form when the page is loaded
+let showForm = false;
+document.querySelector("#lick-button").addEventListener("click", () => {
+  showForm = !showForm;
+  document.querySelector("#adoption-form").style.display = showForm ? "block" : "none";
+  var allPets = document.getElementsByClassName('pet-div');
+  delete allPets
 });
 
 
+// Add an event listener to the form so that when the information is submitted
+// it updates the JSON database with a POST to
 
+const adoptionForm = document.querySelector("#adoption-form")
+adoptionForm.addEventListener("submit", event => {
+    event.preventDefault();
+    fetch("http://localhost:3000/adopters", {
+      method: "POST",
+      headers: {
+        // What we're sending ()
+        "Content-Type": "application/json",
+        // What we want to receive (N/A in this case but setting it as practice)
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        fname: event.target.fname.value,
+        lname: event.target.lname.value,
+        email: event.target.email.value,
+        phone: event.target.phone.value,
+        zipcode: event.target.zipcode.value,
+        details: event.target.details.value
+      })
+    }).then(response => response.json())
+    adoptionForm.reset
+    let name = currentPet.name
+    alert(`${name} thanks you furever! 👅`);
+    document.querySelector("#adoption-form").style.display = "none";
+    renderPets ()
+})
 
-
-
-
-
-// // GLOBAL VARIABLES
-
-// const petNav = document.querySelector("#pet-nav")
-// const searchForm = document.querySelector("#search-form")
-// const searchInput= document.querySelector("#search-query")
-// let currentPet
-
-
-
-// // Write a function to populate pet details when filling into containers and when highlighting 
-// // a particular pet on the right side display panel
-// function petDetails (pet) {
-//     //Set the current pet so that the "lick" button will work
-//     currentPet = pet
-//     //Set the detail photo for when someone clicks on the pet to display details
-//     const pngImage = document.querySelector("#image-png")
-//     pngImage.src = pet.pngImage
-//     pngImage.alt = pet.name
-//     const name = document.querySelector(".name")
-//     name.textContent = "Name: " + pet.name
-//     const age = document.querySelector(".age")
-//     age.textContent = "Age: " + pet.age
-//     const birthday = document.querySelector(".birthday")
-//     birthday.textContent = "Birthday: " + pet.birthday
-//     const breed = document.querySelector(".breed")
-//     breed.textContent = "Breed: " + pet.breed
-//     const tricks = document.querySelector(".tricks")
-//     tricks.textContent = "Tricks: " + pet.tricks
-//     const species = document.querySelector(".species")
-//     species.textContent = "Species: " + pet.species
-//     const sex = document.querySelector(".sex")
-//     sex.textContent = "Sex: " + pet.sex
-//     // const licked = document.querySelector("#licked")
-//     // licked.textContent = pet.licked ? "Licked 👅 ! " : "Lick?"
-//     // const licks = document.querySelector("#licks")
-//     // licks.textContent = pet.licks
-// }
-
-
-
-// // Fetch request to populate the petNav from the JSON file
-// fetch ("http://localhost:3000/pets")
-// .then (response => response.json())
-// .then ((fetchedPets) => {
-//     pets= fetchedPets;
-//     pets.forEach(pet => {
-//         //Make a div for each
-//         const petDiv = document.createElement("div")
-//         petDiv.className = "pet-div"
-//         const image = document.createElement("img")
-//         image.src = pet.image
-//         image.alt = pet.name
-//         // Add event listener to populate right side pane display when user clicks on a pet image
-//         // in the petNav
-//         image.addEventListener("click", () => petDetails(pet))
-//         //Append the image to the div
-//         petDiv.append(image)
-//         //Append each pet div to the petNav using the global constant
-//         petNav.append(petDiv)
-//     })
-//     // Set a starting pet. For now, putting in the first item. But code below is to change this to a
-//     // random pet once we get it working.
-//     // const initialPet = pets[0]
-//     // This code creates a random number for choosing the starting 
-//     function randomIntFromInterval(min, max) { // min and max included 
-//         return Math.floor(Math.random() * (max - min + 1) + min)
-//         }
-//         //Subtract 1 from the randomInt to account for an array value of zero
-//         const petsArrayLengthMinusOne = pets.length -= 1
-//         const randomInt = randomIntFromInterval(0, petsArrayLengthMinusOne)
-        
-//         // console.log("This is my randomInt value: ", randomInt)
-//         // console.log("This is my arrayLength value: ", petsArrayLengthMinusOne)
-//     //Make a constant of the details of the random starting pet
-//     const initialPet = pets[randomInt]
-//     // Call the petDetails function to display details of the array object for the random initial pet
-//     petDetails(initialPet)
-// })
-
-
-
-// //When user clicks on the Lick button, up the number of licks and do a PATCH to the database
-// //to update the number of Licks
-// const lickButton = document.querySelector("#lick-button")
-// lickButton.addEventListener("click", () => {
-//     currentPet.licks++;
-//     currentPet.licked= !currentPet.licked;
-//     fetch(`http://localhost:3000/${currentPet}`, {
-//         method: "PATCH",
-//         headers: {
-//           "Content-Type": "application/json",
-//           Accept: "application/json"
-//         },
-//         body: JSON.stringify({
-//           licks: currentPet.licks,
-//           licked: currentPet.licked
-//         })
-//       })
-//       .then(response => response.json())
-//       // Updating `likes.textContent` here (rather than before our `fetch()`)
-//       //          guarantees that it only updates after our PATCH succeeds.
-//       .then((patchedPet) => (licks.textContent = `${patchedPet.licks} Licks`))
-// })
-
-
-// //For now, since we don't have unique user logins, when someone clicks the lick button,
-// //the button will go away and show up as licked
-
-
-
-// //Make an event listener for the SEARCH
-
-
-// searchForm.addEventListener("submit", (event) => {
-//     event.preventDefault()
-// const searchQuery= searchInput.value.trim().toLowerCase();
-// let pets
-// //filter pets based on search query
-// const filteredPets = pets.filter((pet) => {
-//   const name= pet.name.toLowerCase()
-//   const breed= pet.breed.toLowerCase()
-//   const species=pet.species.toLowerCase()
-//   const sex=pet.sex.toLowerCase()
-//   const age=pet.age.toLowerCase()
-
-//   //return true if properties match the search
-
-//   return breed.includes(searchQuery) || sex.includes(searchQuery) ||species.includes(searchQuery)||age.includes(searchQuery) ||name.includes(searchQuery);
-
-// })
-// //clear current pet display
-// petNav.innerHTML="";
-// // repopulate petNav with filtered pets
-// filteredPets.forEach((pet) => {
-//     const petDiv = document.createElement('div')
-//     petDiv.className= 'pet-div'
-//     const image= document.createElement('img')
-//     image.src = pet.image
-//     image.alt = pet.name
-
-//     image.addEventListener('click', () => petDetails(pet))
-//     petDiv.append(image)
-//     petNav.append(petDiv)
-// })
-
-// //display message, if no filtered pets
-
-
-
-// CODING OFF BELOW AS A START IN CASE WE NEED IT
-// const theButton = document.querySelector("#watched")
-// theButton.addEventListener("click", () => {
-//     //using the global variable, we are making the watched value the opposite of itself
-//     currentMovie.watched = !currentMovie.watched
-//     //Now, on the current Movie, which is the button in this case when we are clicking, we m
-//     theButton.textContent = currentMovie.watched ? "Watched" : "Unwatched"
-//     //Note: For coding challege, fine to just change the text not do a Patch
-
-// if(filteredPets.length ===0) {
-//     const noResultsDiv = document.createElement('div')
-//     noResultsDiv.textContent = 'No pets found matching your search!'
-//     petNav.append(noResultsDiv)
-// }
-
-// })
-
-
-
-
-
-
-// // //CODING OFF BELOW AS A START IN CASE WE NEED IT
-// // const theButton = document.querySelector("#watched")
-// // theButton.addEventListener("click", () => {
-// //     //using the global variable, we are making the watched value the opposite of itself
-// //     currentMovie.watched = !currentMovie.watched
-// //     //Now, on the current Movie, which is the button in this case when we are clicking, we m
-// //     theButton.textContent = currentMovie.watched ? "Watched" : "Unwatched"
-// //     //Note: For coding challege, fine to just change the text not do a Patch
-// // })
